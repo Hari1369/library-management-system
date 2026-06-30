a3dc318faf17d94a34a4cbb53d130ec10eb7c615
Hari1369 <hiranmaymondal369@gmail.com>
2026-06-30 13:45:56 +0000

    add with hashing in password details

--- library_system/settings.py
+++ library_system/settings.py
@@ -12,21 +12,24 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from celery.schedules import crontab
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l_1@1pv3j1ts6x0++7x#-r(gl5mc%o-vtf$21*2jqgj&ec_hqd'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
@@ -81,12 +84,12 @@ WSGI_APPLICATION = 'library_system.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "test",
-        "USER": "quantumd",
-        "PASSWORD": "admlqq",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
--- members/admin.py
+++ members/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.hashers import make_password
 from .models import Librarian
 
 
-admin.site.register(Librarian)
+@admin.register(Librarian)
+class LibrarianAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "name",
+        "surname",
+        "email",
+        "phone_number",
+        "is_active",
+    )
+
+    def save_model(self, request, obj, form, change):
+        if "password" in form.changed_data:
+            obj.password = make_password(obj.password)
+
+        super().save_model(request, obj, form, change)
